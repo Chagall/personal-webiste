8e55b11f6e020363692a62f9d4c18392ccbb8f1f
Fábio A. M. Pereira <famp@operamail.com>
2021-03-20 12:43:54 -0300

    Removed the css that comes with prebuilt into the react project and added a material app bar to start messing with.

---
+++ src/components/SimpleBottomNavigation.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import HomeIcon from "@material-ui/icons/Home";
+//import RestoreIcon from "@material-ui/icons/Restore";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+
+const useStyles = makeStyles({
+  root: {
+    
+    width: "100%",
+  },
+});
+
+export default function SimpleBottomNavigation() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.root}
+    >
+      <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+      <BottomNavigationAction label="Projects" icon={<FavoriteIcon />} />
+      <BottomNavigationAction label="Blog" icon={<LocationOnIcon />} />
+    </BottomNavigation>
+  );
+}
