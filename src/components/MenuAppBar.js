0e9e3ea4a33c933b9c4e59539ab320004f6d823b
Fábio A. M. Pereira <famp@operamail.com>
2021-03-22 21:45:02 -0300

    Setup the MenuAppBar as a menu bar with the possibility to play some music, with play and pause buttons. It now plays the song I put in the music folder when hitting play.

--- src/components/MenuAppBar.js
+++ src/components/MenuAppBar.js
@@ -1,35 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import PauseCircleFilledIcon from "@material-ui/icons/PauseCircleFilled";
 import Typography from "@material-ui/core/Typography";
 import Strings from "./utils/Strings";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     color: theme.palette,
-    flexGrow: 2,
     marginTop: theme.spacing(-1),
     marginLeft: theme.spacing(-1),
-    marginRight: theme.spacing(-1)
+    marginRight: theme.spacing(-1),
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
+  controls: {
+    display: "flex",
+    alignItems: "flex-start",
+    marginLeft: theme.spacing(-2),
   },
   title: {
     flexGrow: 1,
   },
 }));
 
+var musicUrl = "/music/rf_nt.mp3";
+var stream = new Audio(musicUrl);
+stream.preload = "none";
+
 export default function MenuAppBar() {
   const styles = useStyles();
+  let [isPlaying, setPlaying] = useState(false);
+  let [musicName, setMusicName] = useState(Strings.NOTHING);
+
+  const handleClick = () => {
+    if(!isPlaying) {
+      playMusic();
+      setPlaying(true);
+      setMusicName(Strings.RF_NT);
+    }
+    else {
+      pauseMusic();
+      setPlaying(false);
+      setMusicName(Strings.NOTHING);
+    }
+  };
+
+  const playMusic = () => {
+    stream.play();
+  };
+
+  const pauseMusic = () => {
+    stream.pause();
+    stream.src = "";
+    stream.load();
+    
+    stream = null;
+    
+    stream = new Audio();
+    stream.src = musicUrl;
+    stream.preload = "none";
+    stream.pause();
+  };
+
+  const currentButton = () => {
+    if(!isPlaying) {
+      return <PlayArrowIcon />;
+    }
+    else {
+      return <PauseCircleFilledIcon/>;
+    }
+  };
 
   return (
     <div className={styles.root}>
       <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={styles.title}>
-            {Strings.HOME}
+        <Toolbar variant="dense">
+          <div className={styles.controls}>
+            <IconButton
+              color="inherit"
+              aria-label="play"
+              onClick={() => handleClick()}
+            >
+              {currentButton()}
+            </IconButton>
+          </div>
+          <Typography variant="body1" className={styles.title}>
+            {Strings.LISTENING_TO} {musicName}
           </Typography>
         </Toolbar>
       </AppBar>
