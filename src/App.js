0e88f8f3f62ebf3757b95a4744c4b3d6ed9af07c
Fábio A. M. Pereira <famp@operamail.com>
2021-03-24 22:38:56 -0300

    Modified the theme colors to blue and yellow, also changed the background color to a blue gradient.

--- src/App.js
+++ src/App.js
@@ -1,46 +1,32 @@
 import React from "react";
 import "./css/App.css";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
-import cyan from "@material-ui/core/colors/cyan";
-import red from "@material-ui/core/colors/red";
-import MenuAppBar from "./components/MenuAppBar";
+import blue from "@material-ui/core/colors/blue";
+import yellow from "@material-ui/core/colors/yellow";
 import Page from "./components/Page";
+import BottomAppBar from "./components/BottomAppBar";
 
 function App() {
   return (
-    <React.Fragment>
+    <div style={{height: "99vh"}}>
       <ThemeProvider theme={customTheme}>
-        <MenuAppBar />
         <Page />
+        <BottomAppBar />
       </ThemeProvider>
-    </React.Fragment>
+    </div>
   );
 }
 
 const customTheme = createMuiTheme({
   palette: {
-    type: "dark",
+    type: "light",
     primary: {
-      main: cyan[900],
+      main: blue["900"],
     },
     secondary: {
-      main: red[200],
+      main: yellow["A200"],
     },
   },
 });
-
-/*
-
-import Container from "@material-ui/core/Container";
-import SimpleTabs from "./components/SimpleTabs";
-<Container style={containerStyle} maxWidth="md" disableGutters>
-<SimpleTabs />
-</Container>
-
-const containerStyle = {
-  height: "98vh",
-  backgroundColor: "white",
-};
-*/
 
 export default App;
