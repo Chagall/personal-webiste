356a17192f6d9f33d47ccb4a6535b85f58eb8eac
Fábio A. M. Pereira <famp@operamail.com>
2021-03-24 22:39:47 -0300

    Small changes to some strings.

--- src/components/utils/Strings.js
+++ src/components/utils/Strings.js
@@ -1,14 +1,14 @@
 const Strings = Object.freeze({
   STILL_WORKING: "Work in progress...",
   HOME: "Home",
-  LISTENING_TO: "Now playing: ",
+  NOW_PLAYING: "Now playing: ",
   NOTHING: "Nothing... 😴. (Hit play to drop the beats)",
   RF_NT: "Robert Farmer - Nottamun Town",
 
   PRESENTATION: "Hey folks, it's your boy Fábio",
   DESCRIPTION:
     "I'm a Front-end Web Engineer who also knows a thing or two about back-end and Android Development.",
-  DESCRIPTION2: "You can catch me doing stuff and (not) shitposting on the social medias below:",
+  DESCRIPTION2: "You can catch me doing stuff and shitposting on one or more of the social medias below:",
 });
 
 export default Strings;
