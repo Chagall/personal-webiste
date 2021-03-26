11eb174dc6a5cf8d65ff9257d744b5ff472797b4
Fábio A. M. Pereira <famp@operamail.com>
2021-03-25 22:46:27 -0300

    Modified some of the strings used on the program.

--- src/components/utils/Strings.js
+++ src/components/utils/Strings.js
@@ -5,10 +5,16 @@ const Strings = Object.freeze({
   NOTHING: "Nothing... 😴. (Hit play to drop the beats)",
   RF_NT: "Robert Farmer - Nottamun Town",
 
-  PRESENTATION: "Hey folks, it's your boy Fábio",
-  DESCRIPTION:
-    "I'm a Front-end Web Engineer who also knows a thing or two about back-end and Android Development.",
-  DESCRIPTION2: "You can catch me doing stuff and shitposting on one or more of the social medias below:",
+  PRESENTATION: "It's-a Me, Fábio",
+  DESCRIPTION2: "You can catch me doing stuff and probably ",
+  DESCRIPTION3: " on one, or more, of the social medias below.",
+
+  I_AM_A: "I'm a ",
+  FRONT_ENGINEER: "Front-end Web Engineer",
+  WHO_ALSO_KNOWS: " who also knows a thing or two about ",
+  BACKEND: "back-end",
+  AND: " and ",
+  ANDROID_DEV: "Android Development",
 });
 
 export default Strings;
