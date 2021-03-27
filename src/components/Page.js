b207bcc775097bcf0679f371921e2d2b75465f79
Fábio A. M. Pereira <famp@operamail.com>
2021-03-26 23:59:52 -0300

    Modified the page component to alter between the vertical card layout and horitonzal card layout deppending on the browser's current width.

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,22 +1,68 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import MyInfo from "./MyInfo";
+import React, { useEffect, useState } from "react";
+import HorizontalInfoCard from "./HorizontalInfoCard";
+import VerticalInfoCard from "./VerticalInfoCard";
 
 export default function Page() {
-  const styles = useStyles();
 
-  return (
-    <div className={styles.grid}>
-      <MyInfo />
-    </div>
-  );
+  const getBrowserWindowWidth = () => {
+    var windowWidth = 0;
+    if (typeof window.innerWidth == "number") {
+      //Non-IE
+      windowWidth = window.innerWidth;
+    } else if (
+      document.documentElement &&
+      (document.documentElement.clientWidth ||
+        document.documentElement.clientWidth)
+    ) {
+      //IE 6+ in 'standards compliant mode'
+      windowWidth = document.documentElement.clientWidth;
+    } else if (
+      document.body &&
+      (document.body.clientWidth || document.body.clientWidth)
+    ) {
+      //IE 4 compatible
+      windowWidth = document.body.clientWidth;
+    }
+    return windowWidth;
+  };
+
+  const setInfoBasedOnBrowserWidth = () => {
+    let windowWidth = getBrowserWindowWidth();
+    console.log(windowWidth);
+
+    if (currentWidth !== windowWidth) {
+      setCurrentWidth(windowWidth);
+      if (windowWidth < 750) {
+        setInfo(<VerticalInfoCard />);
+      } else {
+        setInfo(<HorizontalInfoCard />);
+      }
+    }
+  };
+
+  const [currentWidth, setCurrentWidth] = useState(-1);
+  const [info, setInfo] = useState(<HorizontalInfoCard />);
+  const [intervalID, setIntervalID] = useState(-1);
+
+  useEffect(() => {
+    setIntervalID(
+      setInterval(() => {
+        setInfoBasedOnBrowserWidth();
+      }, 1000)
+    );
+
+    return function cleanup() {
+      console.log("Limpou o danadinho");
+      clearInterval(intervalID);
+    };
+  }, []);
+
+  return <div style={smallScreen}>{info}</div>;
 }
 
-const useStyles = makeStyles({
-  grid: {
-    marginBottom: "12vh",
-    marginTop: "8vh",
-    marginLeft: "8vw",
-    marginRight: "8vw",
-  },
-});
+
+
+const smallScreen = {
+  textAlign: "center",
+  margin: "0 auto",
+};
