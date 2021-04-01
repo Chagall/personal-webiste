dcc58360635ba8ab719a32445f3d44378751d96e
Fábio A. M. Pereira <famp@operamail.com>
2021-03-31 22:20:59 -0300

    Changed some of the page texts.

--- src/components/utils/Strings.js
+++ src/components/utils/Strings.js
@@ -1,15 +1,18 @@
 const Strings = Object.freeze({
+
+  SITE_NAME: "Commit.blue",
+
   STILL_WORKING: "Work in progress...",
   HOME: "Home",
   NOW_PLAYING: "Now playing: ",
   NOTHING: "Nothing... 😴. (Hit play to drop the beats)",
   RF_NT: "Robert Farmer - Nottamun Town",
 
-  PRESENTATION: "It's-a Me, Fábio",
+  PRESENTATION: "Fabio A.M. Pereira",
   DESCRIPTION2: "You can catch me doing stuff and probably ",
   DESCRIPTION3: " on one, or more, of the social medias below.",
 
-  I_AM_A: "I'm a ",
+  I_AM_A: "What's up y'all 🖖. I'm a ",
   FRONT_ENGINEER: "Front-end Web Engineer",
   WHO_ALSO_KNOWS: " who also knows a thing or two about ",
   BACKEND: "back-end",
