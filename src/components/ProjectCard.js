1ad60ffc0e6ab64bb8786a18c7926b8cf66eb436
Fábio A. M. Pereira <famp@operamail.com>
2021-04-11 18:18:43 -0300

    Modified the Project Card to be able to show a website icon (with a link to the website) in case the project has a website.

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,14 +4,19 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 import IconButton from "@material-ui/core/IconButton";
+import OpenInNewIcon from "@material-ui/icons/OpenInNew";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import Typography from "@material-ui/core/Typography";
 import Constants from "./utils/Constants";
 
-const useStyles = makeStyles(() => ({
-  actions: {
-    textAlign: "center",
+const useStyles = makeStyles((theme) => ({
+  buttonGroupContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    marginTop: theme.spacing(1),
   },
 }));
 
@@ -20,6 +25,7 @@ export default function ProjectCard({
   imgSrc,
   description,
   githubPage,
+  website,
 }) {
   const styles = useStyles();
 
@@ -45,16 +51,30 @@ export default function ProjectCard({
           {description}
         </Typography>
 
-        <div className={styles.actions}>
-          <IconButton
-            aria-label="github-project"
+        <div className={styles.buttonGroupContainer}>
+          <ButtonGroup
             color="primary"
-            onClick={() => {
-              window.open(Constants.GITHUB_PAGE_URL + githubPage);
-            }}
+            aria-label="outlined primary button group"
           >
-            <GitHubIcon />
-          </IconButton>
+            <IconButton
+              style={{ display: website ? "inline-block" : "none" }}
+              aria-label="website"
+              onClick={() => {
+                window.open(website);
+              }}
+            >
+              <OpenInNewIcon />
+            </IconButton>
+
+            <IconButton
+              aria-label="github-project"
+              onClick={() => {
+                window.open(Constants.GITHUB_PAGE_URL + githubPage);
+              }}
+            >
+              <GitHubIcon />
+            </IconButton>
+          </ButtonGroup>
         </div>
       </CardContent>
     </Card>
